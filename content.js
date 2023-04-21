54273094aad24cbff4a271ef32bb7959ea2fe534
Miha Stele <mihas@amebis.doma>
2023-04-21 13:07:37 +0200

    Create bootstrap modal

--- content.js
+++ content.js
@@ -1,1 +1,73 @@
-alert("Warning: You are visiting a social media site!");
+// alert("Warning: You are visiting a social media site!");
+
+
+let modal = document.createElement("div");
+modal.id = "modal";
+modal.style.position = "fixed";
+modal.style.top = "0";
+modal.style.left = "0";
+modal.style.width = "100%";
+modal.style.height = "100%";
+modal.style.backgroundColor = "rgba(0,0,0,0.5)";
+modal.style.zIndex = "1000";
+modal.style.display = "flex";
+modal.style.justifyContent = "center";
+modal.style.alignItems = "center";
+
+
+let modalContent = document.createElement("div");
+modalContent.style.backgroundColor = "#fff";
+modalContent.style.padding = "20px";
+modalContent.style.borderRadius = "5px";
+modalContent.style.width = "500px";
+modalContent.style.height = "300px";
+modalContent.style.display = "flex";
+modalContent.style.flexDirection = "column";
+modalContent.style.justifyContent = "center";
+modalContent.style.alignItems = "center";
+
+let modalHeader = document.createElement("h1");
+modalHeader.innerText = "Warning: You are visiting a social media site!";
+modalHeader.style.margin = "0";
+modalHeader.style.padding = "0";
+modalHeader.style.color = "#000";
+    
+let modalBody = document.createElement("p");
+modalBody.innerText = "You are about to visit a social media site. Are you sure you want to continue?";
+modalBody.style.margin = "0";
+modalBody.style.padding = "0";
+modalBody.style.color = "#000";
+
+let modalFooter = document.createElement("div");
+modalFooter.style.display = "flex";
+modalFooter.style.justifyContent = "space-around";
+modalFooter.style.width = "100%";
+modalFooter.style.marginTop = "20px";
+
+let yesButton = document.createElement("button");
+yesButton.innerText = "Yes";
+yesButton.style.padding = "10px 20px";
+yesButton.style.borderRadius = "5px";
+yesButton.style.border = "none";
+yesButton.style.backgroundColor = "#4CAF50";
+yesButton.style.color = "#fff";
+yesButton.style.cursor = "pointer";
+
+let noButton = document.createElement("button");
+noButton.innerText = "No";
+noButton.style.padding = "10px 20px";
+noButton.style.borderRadius = "5px";
+noButton.style.border = "none";
+noButton.style.backgroundColor = "#f44336";
+noButton.style.color = "#fff";
+noButton.style.cursor = "pointer";
+
+modalFooter.appendChild(yesButton);
+modalFooter.appendChild(noButton);
+modalContent.appendChild(modalHeader);
+modalContent.appendChild(modalBody);
+modalContent.appendChild(modalFooter);
+modal.appendChild(modalContent);
+document.body.appendChild(modal);
+
+$("#modal").show();
